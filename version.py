9a76406aa54c0adc0862eb15d44075ebfdc6a7e3
beegica <beegica@gmail.com>
2022-01-12 13:45:21 -0500

    versionupdate

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-__version__ = "6bddfb9"
-__author__ = "beegica"
-__email__ = "beegica@gmail.com"
-__date__ = "Fri Jan 7 15:51:03 2022 -0500"
+__version__ = ""
+__author__ = ""
+__email__ = ""
+__date__ = ""
