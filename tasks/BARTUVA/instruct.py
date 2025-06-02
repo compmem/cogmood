9869f4ce660db4fc8098d9c739d60500e75c4ff6
Dylan Nielson <adenosine@gmail.com>
2025-06-02 10:58:24 -0400

    fix practice skip

--- tasks/BARTUVA/instruct.py
+++ tasks/BARTUVA/instruct.py
@@ -36,7 +36,6 @@ def Instruct(self, config, run_num, sub_dir, task_dir=None,
         cont_key_str = str(config.CONT_KEY[0])
 
     practice_inst = Func(get_practice_inst, config, run_num).result
-
     if config.TOUCH:
         with Loop(computer_list) as instruction:
             txt = instruction.current
@@ -165,103 +164,102 @@ def Instruct(self, config, run_num, sub_dir, task_dir=None,
                             GetResponse(keys=config.CONT_KEY)
 
         with Else():
-            with If(is_first):
-                with Parallel():
-                    MouseCursor(blocking=False)
-                    with Serial(blocking=False):
-                        with If(full_instructions):
-                            with Loop(computer_list) as instruction:
-                                txt = instruction.current
-                                with Parallel():
-                                    with If((instruction.i == 2)):
-                                        with Parallel():
-                                            img2 = Image(source=config.INST2_IMG_PATH,
-                                                         bottom=(self.exp.screen.height / 2.) + s(50),
-                                                         keep_ratio=True, allow_stretch=True,
-                                                         height=s(400))
-                                            lbl2 = Label(text=txt % (config.KEY_TEXT[0],
-                                                                     config.KEY_TEXT[-1]),
-                                                         halign='left', top=img2.bottom - s(10),
-                                                         font_size=s(config.LABEL_FONT_SIZE))
-                                            Label(text='Press %s to continue' % (config.CONT_KEY_STR),
-                                                  halign='left',
-                                                  top=lbl2.bottom,
-                                                  font_size=s(config.LABEL_FONT_SIZE))
-                                    with Else():
-                                        with Parallel():
-                                            lbl1 = Label(text=txt,
-                                                         halign='left',
-                                                         font_size=s(config.LABEL_FONT_SIZE))
-                                            Label(text='Press %s to continue' % (config.CONT_KEY_STR),
-                                                  halign='left',
-                                                  top=lbl1.bottom - s(75),
-                                                  font_size=s(config.LABEL_FONT_SIZE))
-                                with UntilDone():
-                                    Wait(.5)
-                                    GetResponse(keys=config.CONT_KEY)
-
-                        with If(practice):
-                            Label(text=practice_inst,
-                                  halign='center',
-                                  font_size=s(config.LABEL_FONT_SIZE))
+            with Parallel():
+                MouseCursor(blocking=False)
+                with Serial(blocking=False):
+                    with If(full_instructions):
+                        with Loop(computer_list) as instruction:
+                            txt = instruction.current
+                            with Parallel():
+                                with If((instruction.i == 2)):
+                                    with Parallel():
+                                        img2 = Image(source=config.INST2_IMG_PATH,
+                                                     bottom=(self.exp.screen.height / 2.) + s(50),
+                                                     keep_ratio=True, allow_stretch=True,
+                                                     height=s(400))
+                                        lbl2 = Label(text=txt % (config.KEY_TEXT[0],
+                                                                 config.KEY_TEXT[-1]),
+                                                     halign='left', top=img2.bottom - s(10),
+                                                     font_size=s(config.LABEL_FONT_SIZE))
+                                        Label(text='Press %s to continue' % (config.CONT_KEY_STR),
+                                              halign='left',
+                                              top=lbl2.bottom,
+                                              font_size=s(config.LABEL_FONT_SIZE))
+                                with Else():
+                                    with Parallel():
+                                        lbl1 = Label(text=txt,
+                                                     halign='left',
+                                                     font_size=s(config.LABEL_FONT_SIZE))
+                                        Label(text='Press %s to continue' % (config.CONT_KEY_STR),
+                                              halign='left',
+                                              top=lbl1.bottom - s(75),
+                                              font_size=s(config.LABEL_FONT_SIZE))
                             with UntilDone():
                                 Wait(.5)
                                 GetResponse(keys=config.CONT_KEY)
 
-                            number_of_sets = 1
-                            self.set_number = 0
-                            self.grand_total = config.GRAND_TOTAL
-                            self.balloon_number_session = 0
-                            self.trkp_press_time = None
-
-                            Wait(1.)
-                            # Loop over practice blocks
-                            with Loop(number_of_sets):
-                                Wait(.5, jitter=.5)
-
-                                # Calling listgen as 'bags'
-                                bg = Func(add_air,
-                                          total_number_of_balloons=len(config.PRACTICE_SETUP) * 2,
-                                          num_ranges=len(config.PRACTICE_SETUP),
-                                          balloon_setup=config.PRACTICE_SETUP,
-                                          randomize=config.RANDOMIZE_BALLOON_NUM,
-                                          reward_low=config.REWARD_LOW,
-                                          reward_high=config.REWARD_HIGH,
-                                          subject_directory=sub_dir,
-                                          practice=True,
-                                          shuffle_bags=config.SHUFFLE_BAGS)
-                                bags = bg.result
-                                self.block_tic = 0
-
-                                # with Loop(bag.current) as balloon:
-                                with Loop(bags) as balloon:
-                                    Balloon = BARTSub(config,
-                                                      log_name='bart_practice',
-                                                      balloon=balloon.current,
-                                                      block=self.block_tic,
-                                                      set_number=self.set_number,
-                                                      grand_total=self.grand_total,
-                                                      balloon_number_session=self.balloon_number_session,
-                                                      subject=self._exp.subject,
-                                                      run_num=run_num,
-                                                      trkp_press_time=self.trkp_press_time)
-                                    self.balloon_number_session += 1
-                                    self.grand_total = Balloon.grand_total
-                                self.block_tic += 1
-
-                                self.set_number += 1
-
+                    with If(practice):
+                        Label(text=practice_inst,
+                              halign='center',
+                              font_size=s(config.LABEL_FONT_SIZE))
+                        with UntilDone():
                             Wait(.5)
-                            Label(text='You have completed the practice.\n' +
-                                       'Press %s to continue.' % (cont_key_str),
-                                  halign='center',
-                                  font_size=s(config.LABEL_FONT_SIZE))
-                            with UntilDone():
-                                Wait(0.5)
-                                GetResponse(keys=config.CONT_KEY)
-                    with Serial(blocking=False):
-                        with ButtonPress():
-                            Button(text="Skip Practice", width=s(config.SKIP_SIZE[0]),
-                                   bottom=0, right=self.exp.screen.width,
-                                   height=s(config.SKIP_SIZE[1]),
-                                   font_size=s(config.SKIP_FONT_SIZE))
+                            GetResponse(keys=config.CONT_KEY)
+
+                        number_of_sets = 1
+                        self.set_number = 0
+                        self.grand_total = config.GRAND_TOTAL
+                        self.balloon_number_session = 0
+                        self.trkp_press_time = None
+
+                        Wait(1.)
+                        # Loop over practice blocks
+                        with Loop(number_of_sets):
+                            Wait(.5, jitter=.5)
+
+                            # Calling listgen as 'bags'
+                            bg = Func(add_air,
+                                      total_number_of_balloons=len(config.PRACTICE_SETUP) * 2,
+                                      num_ranges=len(config.PRACTICE_SETUP),
+                                      balloon_setup=config.PRACTICE_SETUP,
+                                      randomize=config.RANDOMIZE_BALLOON_NUM,
+                                      reward_low=config.REWARD_LOW,
+                                      reward_high=config.REWARD_HIGH,
+                                      subject_directory=sub_dir,
+                                      practice=True,
+                                      shuffle_bags=config.SHUFFLE_BAGS)
+                            bags = bg.result
+                            self.block_tic = 0
+
+                            # with Loop(bag.current) as balloon:
+                            with Loop(bags) as balloon:
+                                Balloon = BARTSub(config,
+                                                  log_name='bart_practice',
+                                                  balloon=balloon.current,
+                                                  block=self.block_tic,
+                                                  set_number=self.set_number,
+                                                  grand_total=self.grand_total,
+                                                  balloon_number_session=self.balloon_number_session,
+                                                  subject=self._exp.subject,
+                                                  run_num=run_num,
+                                                  trkp_press_time=self.trkp_press_time)
+                                self.balloon_number_session += 1
+                                self.grand_total = Balloon.grand_total
+                            self.block_tic += 1
+
+                            self.set_number += 1
+
+                        Wait(.5)
+                        Label(text='You have completed the practice.\n' +
+                                   'Press %s to continue.' % (cont_key_str),
+                              halign='center',
+                              font_size=s(config.LABEL_FONT_SIZE))
+                        with UntilDone():
+                            Wait(0.5)
+                            GetResponse(keys=config.CONT_KEY)
+                with Serial(blocking=False):
+                    with ButtonPress():
+                        Button(text="Skip Practice", width=s(config.SKIP_SIZE[0]),
+                               bottom=0, right=self.exp.screen.width,
+                               height=s(config.SKIP_SIZE[1]),
+                               font_size=s(config.SKIP_FONT_SIZE))
