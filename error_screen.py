3e3bb11126692e769cfb20fa14f08218b0f1ee12
Dylan Nielson <adenosine@gmail.com>
2025-07-23 11:21:15 -0400

    move error message down the screen a bit

--- error_screen.py
+++ error_screen.py
@@ -3,7 +3,7 @@ from config import SSI_FONT_SIZE
 
 @Subroutine
 def error_screen(self, error, message):
-    with BoxLayout(orientation='vertical', spacing=20):
+    with BoxLayout(orientation='vertical', spacing=20, pos=[self.exp.screen.width/2, self.exp.screen.height/4]):
         Label(name="error_details",
               text=error,
               text_size=(scale(700), None), font_size=scale(SSI_FONT_SIZE),
